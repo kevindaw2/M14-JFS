3d0f7488fdaca8e12d4c14ce8c6b84776dcba388
kevindaw2 <91140716+kevindaw2@users.noreply.github.com>
2022-01-18 18:58:50 +0100

    ex27 JSF

---
+++ exercici27/src/main/java/com/mycompany/exercici27/ajaxBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exercici27;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author kguina.daw2n
+ */
+@Named(value = "ajaxBean")
+@Dependent
+public class ajaxBean {
+    
+    public ajaxBean() {
+    }
+    
+}
